0ebeabf942234bb8566fcbb39cac005a5bec3566
Victor Okpe <okpevictor84@gmail.com>
2024-01-20 18:01:31 -0800

    ff

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""This module defines a class to manage file storage for hbnb clone."""
 import json
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """This class manages storage of hbnb models in JSON format."""
+
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
+    def all(self, cls=None):
+        """Return a dictionary of models currently in storage."""
+        if cls is not None:
+            cls_all = {}
+            for key, value in FileStorage.__objects.items():
+                if cls == type(value):
+                    cls_all[key] = value
+            return cls_all
         return FileStorage.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """Add new object to storage dictionary."""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """Save storage dictionary to file."""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
-            json.dump(temp, f)
+            json.dump(temp, f, indent=4)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Load storage dictionary from file."""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -45,6 +52,15 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete obj from __objects."""
+        if obj is not None:
+            fs = FileStorage()
+            for key, value in FileStorage.__objects.items():
+                if value == obj:
+                    del FileStorage.__objects[key]
+                    return
--- models/place.py
+++ models/place.py
@@ -1,18 +1,60 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, DateTime
+from datetime import datetime
+from sqlalchemy.orm import relationship
+from .review import Review
+from .amenity import Amenity
+import os
 
-
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    reviews = relationship('Review', backref='places', cascade='all, delete-orphan')
     amenity_ids = []
+
+    @property
+    def reviews(self):
+        """Returns a list of Review intances that have the same place_id as Place.id."""
+        from models import storage
+        all_reviews_dict = storage.all(Review)
+        reviews_list = []
+        for review in all_reviews_dict.keys():
+            if Review.id in review:
+                reviews_list.append(all_reviews_dict[review])
+        return reviews_list
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        amenities = relationship('Amenity', secondary='place_amenity', viewonly=False)
+    else:
+        @property
+        def amenities(self):
+            """Getter attribute amenities that returns the list of Amenity instances based on the attribute amenity_ids"""
+            from models import storage
+
+            amenities_list = []
+            for amenity_id in self.amenity_ids:
+                amenity = storage.get(Amenity, amenity_id)
+                if amenity:
+                    amenities_list.append(amenity)
+            return amenities_list
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter attribute amenities that handles append method for adding \
+            an Amenity.id to the attribute amenity_ids"""
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
